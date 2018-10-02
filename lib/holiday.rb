1c4316f19ee1ea5f80909b00a7d1da94ecfac47a
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-02 01:17:38 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.select do |key, value|
   if value.include? "BBQ"
-    puts key 
+    return key 
   end 
 end
 end 
