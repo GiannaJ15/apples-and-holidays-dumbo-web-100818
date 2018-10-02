2182f3a79c70bd8a851040533b36dc3c282d63f6
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-02 00:35:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |key, value|
   puts "#{key.capitalize}:"
   value.each do |holiday, supply|
-  puts "  #{holiday.to_s.titlecase.gsub("_"," ")}: #{supply.join(", ")}"
+     puts "  #{holiday.to_s.titlecase.gsub("_"," ")}: #{supply.join(", ")}"
 end
 end 
 end 
