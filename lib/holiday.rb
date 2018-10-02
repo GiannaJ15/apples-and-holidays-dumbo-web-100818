82d9550ff850eec9dd03fa7e3ffc4764b322f588
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-10-02 00:11:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,9 +60,13 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  
+  holiday_hash.each do |key, value|
+  puts "#{key.capitalize}:"
+  value.each do |holiday, supply|
+  puts "  #{holiday.capitalize}: #{supply.join(", ")}"
 end
-end
+end 
+end 
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
